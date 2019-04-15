f8ed5d2d4608839b0162a6a82fecbcb6db8576e3
Joe Greubel <joegreubel1@gmail.com>
2019-04-14 21:23:35 -0700

    Init

---
+++ src/main/kotlin/com/jgreubel/allkotlin/AllKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.jgreubel.allkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AllKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/jgreubel/allkotlin/AllKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jgreubel.allkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AllKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
