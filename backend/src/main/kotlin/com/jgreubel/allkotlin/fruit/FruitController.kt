4784fb35f521eb31700dc2daf0e069cb64f17f18
Joe Greubel <joegreubel1@gmail.com>
2019-04-17 22:10:47 -0700

    Remove open from spring component!

--- backend/src/main/kotlin/com/jgreubel/allkotlin/fruit/FruitController.kt
+++ backend/src/main/kotlin/com/jgreubel/allkotlin/fruit/FruitController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/fruit")
-open class FruitController {
+class FruitController {
 
     val fruits: MutableList<Fruit> = mutableListOf()
 
