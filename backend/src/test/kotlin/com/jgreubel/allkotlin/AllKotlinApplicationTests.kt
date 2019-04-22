43a700db149cf7fba2d0d7937fce3f1fca1ecb48
Joe Greubel <joegreubel1@gmail.com>
2019-04-21 21:01:13 -0700

    Fix styling issues in useless test

--- backend/src/test/kotlin/com/jgreubel/allkotlin/AllKotlinApplicationTests.kt
+++ backend/src/test/kotlin/com/jgreubel/allkotlin/AllKotlinApplicationTests.kt
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class AllKotlinApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
